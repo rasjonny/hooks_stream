c9d8164058a143300c6346bc28ac67e8644a7047
unknown <rasjonnysol06@gmail.com>
2022-11-23 12:27:49 +0300

    ImageLoader

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
@@ -10,18 +11,31 @@ void main() {
   ));
 }
 
-Stream<String> getTime() => Stream.periodic(const Duration(seconds: 1),
-    ((computationCount) => DateTime.now().toIso8601String()));
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(transform ?? (e) => e).where((e) => e != null).cast();
+}
+
+const url =
+    'https://cdn.britannica.com/26/58626-050-3B2B1DA0/Simien-Mountains-Ethiopian-Plateau-Ethiopia.jpg';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dateTime = useStream(getTime());
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)));
+    final snapshot = useFuture(future);
     return Scaffold(
-      appBar: AppBar(title: Text(dateTime.data ?? "homepage")),
-      body: const Text(''),
+      appBar: AppBar(title: const Text("homepage")),
+      body: Column(
+        children: [snapshot.data].compactMap().toList(),
+      ),
     );
   }
 }
